40b0ba7b0a17393fc7e0655e5a070349485008a5
gitCraze2020 <jurgenstoop@yahoo.com>
2020-06-11 14:21:30 -0400

    sec

--- 12-git-add-origin-push.sh
+++ 12-git-add-origin-push.sh
@@ -13,9 +13,9 @@ TMP_CREATE_REPO='{"name":"'"$TMP_REPO_NAME"'"}'
 TMP_CREATE_CMD="curl -u $TMP_GIT_USER_NAME https://api.github.com/user/repos -d $TMP_CREATE_REPO"
 $TMP_CREATE_CMD
 
-TMP_GIT_NAME="https://github.com/gitCraze2020/$TMP_REPO_NAME.git"
+TMP_GIT_URL="https://github.com/gitCraze2020/$TMP_REPO_NAME.git"
 
-git remote add origin $TMP_GIT_NAME
+git remote add origin $TMP_GIT_URL
 
 git add --all
 
