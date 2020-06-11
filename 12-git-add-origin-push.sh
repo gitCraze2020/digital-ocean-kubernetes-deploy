2d4591652575679bc74ddd3ceb0526a6fd429bfa
gitCraze2020 <jurgenstoop@yahoo.com>
2020-06-11 14:12:14 -0400

    initial commit

--- 12-git-add-origin-push.sh
+++ 12-git-add-origin-push.sh
@@ -8,13 +8,14 @@ git config --global user.name  "$TMP_GIT_USER_NAME"
 TMP_REPO_NAME=digital-ocean-kubernetes-deploy
 
 # create new repository
+# WARNING this is a depricated API (2020)
 TMP_CREATE_REPO='{"name":"'"$TMP_REPO_NAME"'"}'
 TMP_CREATE_CMD="curl -u $TMP_GIT_USER_NAME https://api.github.com/user/repos -d $TMP_CREATE_REPO"
 $TMP_CREATE_CMD
 
 TMP_GIT_NAME="https://github.com/gitCraze2020/$TMP_REPO_NAME.git"
 
-git remote add origin $TMP_GIT_USER_NAME/$TMP_GIT_NAME
+git remote add origin $TMP_GIT_NAME
 
 git add --all
 
