2235c34a657f6a937630e67dfbe44a809bde0ee1
gitCraze2020 <jurgenstoop@yahoo.com>
2020-06-16 18:22:50 -0400

    initial commit

--- 15-git-add-origin-push.sh
+++ 15-git-add-origin-push.sh
@@ -6,10 +6,10 @@ TMP_REPO_NAME="digital-ocean-kubernetes-deploy"
 TMP_GIT_URL="https://github.com/$TMP_GIT_USER_NAME/$TMP_REPO_NAME.git"
 echo $TMP_GIT_URL
 
-git remote add origin $TMP_GIT_URL
+#git remote add origin $TMP_GIT_URL
 
 git add --all
-
+	
 git commit -m "initial commit"
 
 git push -u origin master
