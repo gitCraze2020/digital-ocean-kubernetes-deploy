f9936be102219e2188e046f1dedce1ae79afa2cb
gitCraze2020 <jurgenstoop@yahoo.com>
2020-06-16 18:28:12 -0400

    initial commit

--- 15-git-add-origin-push.sh
+++ 15-git-add-origin-push.sh
@@ -3,10 +3,10 @@
 # the authentication is only needed once to set config
 TMP_GIT_USER_NAME=gitCraze2020
 git config --global user.email "jurgenstoop@yahoo.com"
-git config --global user.name  "$TMP_GIT_USER_NAME"
+git config --global user.name "$TMP_GIT_USER_NAME"
 
 # create new repository
-TMP_GIT_USER_NAME="gitCraze2020"
+TMP_GIT_USER_NAME=gitCraze2020
 TMP_REPO_NAME="digital-ocean-kubernetes-deploy"
 TMP_GIT_URL="https://github.com/$TMP_GIT_USER_NAME/$TMP_REPO_NAME.git"
 echo $TMP_GIT_URL
